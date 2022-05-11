d587887fee6ebadad081860e0fa968c50e42de13
Jahnavi-Chunduru <96530495+Jahnavi-Chunduru@users.noreply.github.com>
2022-05-11 15:18:09 +0530

    Add files via upload

---
+++ jump game23.py
@@ -0,0 +1,10 @@
+print("enter the elements of array:")
+nums=list(map(int,input().split()))
+x=nums[1]
+for i in range(0,len(nums)):
+     if(1+nums[1]==len(nums)-1):
+         print("true")
+         break
+     else:
+         print("false")
+         break
