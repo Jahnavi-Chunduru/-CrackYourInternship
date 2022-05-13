9b2dbb43caf649e3a62e7eaf223062221dd5cf48
Jahnavi-Chunduru <96530495+Jahnavi-Chunduru@users.noreply.github.com>
2022-05-13 18:54:10 +0530

    Update ceiling in a sorted array79.py

--- ceiling in a sorted array79.py
+++ ceiling in a sorted array79.py
@@ -1,6 +1,4 @@
 arr=list(map(int,input().split()))
 k=int(input("enter a value to find it's ceiling"))
 newarr=[i for i in arr if i>k]
-print(newarr)
 print("the ceiling of",k,"is",min(newarr))
-print(arr)
