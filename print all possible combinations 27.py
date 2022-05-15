fb10e543020d941f69e5a66196e9f8927995859a
Jahnavi-Chunduru <96530495+Jahnavi-Chunduru@users.noreply.github.com>
2022-05-15 17:14:34 +0530

    Update print all possible combinations 27.py

--- print all possible combinations 27.py
+++ print all possible combinations 27.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 def combine(arr,s):
     return list(combinations(arr,s))
+print("Enter the elements of list:")
 nums=list(map(int,input().split()))
 k=int(input("Sets in..."))
 print(combine(nums,k))
