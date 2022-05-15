c0d173033193b67712c6bcfbbd4c451fa6bb6e2f
Jahnavi-Chunduru <96530495+Jahnavi-Chunduru@users.noreply.github.com>
2022-05-15 17:11:09 +0530

    Add files via upload

---
+++ print all possible combinations 27.py
@@ -0,0 +1,6 @@
+from itertools import combinations
+def combine(arr,s):
+    return list(combinations(arr,s))
+nums=list(map(int,input().split()))
+k=int(input("Sets in..."))
+print(combine(nums,k))
