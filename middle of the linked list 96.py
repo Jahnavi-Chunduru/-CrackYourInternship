1f8645db1417457bab72d141adb21d545e8b8baa
Jahnavi-Chunduru <96530495+Jahnavi-Chunduru@users.noreply.github.com>
2022-05-23 15:36:29 +0530

    Add files via upload

---
+++ middle of the linked list 96.py
@@ -0,0 +1,7 @@
+class solution:
+    def middlenode(self,head:ListNode)->ListNode:
+        arr=[head]
+        while(arr[-1].next):
+            arr.append(arr[-1].next)
+        return arr[len(arr)//2]
+    
