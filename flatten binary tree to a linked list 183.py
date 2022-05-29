0c7a884a90f09571caadfb482885f1228eca3f46
Jahnavi-Chunduru <96530495+Jahnavi-Chunduru@users.noreply.github.com>
2022-05-29 22:23:28 +0530

    Add files via upload

---
+++ flatten binary tree to a linked list 183.py
@@ -0,0 +1,9 @@
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        curr = root
+        while curr:
+            if curr.left:
+                runner = curr.left
+                while runner.right: runner = runner.right
+                runner.right, curr.right, curr.left = curr.right, curr.left, None
+            curr = curr.right
