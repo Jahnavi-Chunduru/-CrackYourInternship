df58d05a73a1d7b9d9d7f249ffb892cff74ee7fd
Jahnavi-Chunduru <96530495+Jahnavi-Chunduru@users.noreply.github.com>
2022-05-29 22:43:48 +0530

    Add files via upload

---
+++ kth smallest element in bst 186.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def kthSmallest(self, root, k):
+        s, cur, rank = [], root, 0
+
+        while s or cur:
+            if cur:
+                s.append(cur)
+                cur = cur.left
+            else:
+                cur = s.pop()
+                rank += 1
+                if rank == k:
+                    return cur.val
+                cur = cur.right
+
+        return float("-inf")
+from itertools import islice
+class Solution2(object):
+    def kthSmallest(self, root, k):
+        def gen_inorder(root):
+            if root:
+                for n in gen_inorder(root.left):
+                    yield n
+
+                yield root.val
+
+                for n in gen_inorder(root.right):
+                    yield n
+
+        return next(islice(gen_inorder(root), k-1, k))
